b3f74b7ee88722fc79385025f77d5a43a19fd3f8
ShubhamPrasad-TransEV <transmogrify11@outlook.com>
2024-03-19 11:29:58 +0530

    HRMS repo Initialized and Angular added

---
+++ HRMS-FE/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { ForgotPasswordComponent } from './components/authentication/forgot-password/forgot-password.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
