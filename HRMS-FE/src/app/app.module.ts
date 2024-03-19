a05f51fbb8f14a9949335e57add377c0567c7a2f
ShubhamPrasad-TransEV <transmogrify11@outlook.com>
2024-03-19 11:34:53 +0530

    navbar added

--- HRMS-FE/src/app/app.module.ts
+++ HRMS-FE/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/authentication/login/login.component';
 import { ForgotPasswordComponent } from './components/authentication/forgot-password/forgot-password.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
