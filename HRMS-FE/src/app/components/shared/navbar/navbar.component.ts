89b9981e8991b423864e4dcceb7a1a74d7a919ca
sagniktransmogrify <transmogrify17@outlook.com>
2024-03-27 12:28:10 +0530

    navdropdownfix

--- HRMS-FE/src/app/components/shared/navbar/navbar.component.ts
+++ HRMS-FE/src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -11,11 +12,42 @@ export class NavbarComponent {
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
-
+  
   closeDropdown() {
     this.isDropdownOpen = false;
   }
-  logout(){
-
+  logout() {
+    // const authToken = sessionStorage.getItem('authToken');
+    // this.submitted = true;
+    // if (authToken) {
+    //   setTimeout(() => {
+    //     this.authService.logout(authToken as string).subscribe(response => {
+    //       if (response || response.success) {
+    //         this.router.navigate(['/login']);
+    //         sessionStorage.removeItem('authToken');
+    //         this.toastr.success('Logged out successfully', '', {
+    //           positionClass: 'toast-bottom-center'
+    //         });
+    //       } else {
+    //         console.error('Logout failed:', response && response.error ? response.error : 'Unknown error');
+    //         this.submitted = false;
+    //       }
+    //     }, error => {
+    //       console.error('An error occurred during logout:', error);
+    //       this.submitted = false;
+    //     });
+    //   }, 1000);
+    // } else {
+    //   console.warn('No auth token found in session storage');
+    //   this.router.navigate(['/login']);
+    //   this.submitted = false;
+    // }
   }
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: MouseEvent) {
+    if (!(event.target as HTMLElement).closest('.profile-dropdown')) {
+      this.closeDropdown();
+    }
+  }
+
 }
