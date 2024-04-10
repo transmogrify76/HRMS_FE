94ac6b1a95b413fa0e06fff48b755d286c6855e2
ShubhamPrasad-TransEV <transmogrify11@outlook.com>
2024-04-10 15:28:16 +0530

    My Profile updated

--- HRMS-FE/src/app/components/my-profile/my-profile.component.ts
+++ HRMS-FE/src/app/components/my-profile/my-profile.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HrmsApiService } from 'src/app/services/hrms-api.service';
 
-export interface Employee {
-  id: number;
-  username: string;
-  firstname: string;
-  lastname: string;
-  email: string;
-}
 
 @Component({
   selector: 'app-my-profile',
@@ -15,23 +8,23 @@ export interface Employee {
   styleUrls: ['./my-profile.component.scss']
 })
 export class MyProfileComponent implements OnInit {
-  employeeDetails: Employee = {} as Employee;
+  employeeDetails: any;
+  empId : number = 0;
 
   constructor(private http: HrmsApiService) { }
 
   ngOnInit(): void {
-    // Call fetchEmployeeDetails function when the component initializes
     this.fetchEmployeeDetails();
   }
-
+  
   fetchEmployeeDetails(): void {
-    const selectedEmployeeId = 1; 
+    this.empId = Number(sessionStorage.getItem('empId'));
 
-    // Call the service function to fetch employee details
-    this.http.getEmployeeDetails(selectedEmployeeId).subscribe(
-      (employee: Employee) => {
-        // Update employeeDetails with the response from the service
+    this.http.getEmployeeDetails(this.empId).subscribe(
+      (employee) => {
         this.employeeDetails = employee;
+        console.log('====-----====' , this.employeeDetails.employee);
+        
       },
       (error) => {
         console.error('Error fetching employee details:', error);
