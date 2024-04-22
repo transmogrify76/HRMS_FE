16bde3b0bdc43c7d87af370b387dd85cc4708c4d
Chitradeep <transev76@gmail.com>
2024-04-22 11:57:28 +0530

    time pipe

--- HRMS-FE/src/app/time.pipe.ts
+++ HRMS-FE/src/app/time.pipe.ts
@@ -7,7 +7,7 @@ export class SubtractHoursPipe implements PipeTransform {
   transform(value: any): any {
     if (!value) return value;
     const date = new Date(value);
-    date.setHours(date.getHours() - 6); 
+    date.setHours(date.getHours() - 5); 
     date.setMinutes(date.getMinutes() - 30); 
     return date;
   }
