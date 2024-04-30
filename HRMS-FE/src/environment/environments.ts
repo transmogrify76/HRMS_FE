75cbeb968d95b6ab390c19fdb3d0e63c8aed2f7c
ShubhamPrasad-TransEV <transmogrify11@outlook.com>
2024-04-30 15:29:05 +0530

    apiUrl changed

--- HRMS-FE/src/environment/environments.ts
+++ HRMS-FE/src/environment/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'http://localhost:3000/hrms',
-    // apiUrl: 'https://hrms-backend-noea.onrender.com/hrms'
+    // apiUrl: 'http://localhost:3000/hrms',
+    apiUrl: 'https://hrms-backend-noea.onrender.com/hrms'
 };
